95672d3ca0d8a230300f3bdbb02ac1f21b5e72da
JulianMolina09 <soccerj703@gmail.com>
2023-01-04 08:46:39 -0500

    webville

---
+++ webvilletunes/playlist.js
@@ -0,0 +1,14 @@
+window.onload = init;
+function init() {
+var button = document.getElementById("addButton"); button.onclick = handleButtonClick;
+
+}
+
+function handleButtonClick() {
+    var textInput = document.getElementById("songTextInput");
+    var songName = textInput.value;
+    var li = document.createElement("li"); 
+    li.innerHTML = songName;
+    var ul = document.getElementById("playlist"); 
+    ul.appendChild(li);
+}
