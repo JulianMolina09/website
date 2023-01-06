41d5c3d11ed16232ac4731ffcbe56b9c83b9385e
JulianMolina09 <soccerj703@gmail.com>
2023-01-06 08:28:37 -0500

    WebVILLLEEEEEEEEEE

--- webvilletunes/playlist.js
+++ webvilletunes/playlist.js
@@ -1,24 +1,35 @@
 window.onload = init;
-function init() {
-var button = document.getElementById("addButton"); button.onclick = handleButtonClick;
-
-}
-
-function handleButtonClick() {
-    var textInput = document.getElementById("songTextInput");
-    var songName = textInput.value;
-    var li = document.createElement("li"); 
-    li.innerHTML = songName;
-    var ul = document.getElementById("playlist"); 
-    ul.appendChild(li);
-}
 
 function init() {
-    var loc = window.location.href;
-    var HTMLvalidLinkStr = 'http://validator.w3.org/check?uri=' + loc;
-    var CSSvalidLinkStr = 'http://jigsaw.w3.org/css-validator/validator?uri=' +
-                          loc + '?profile=css3';
-    document.getElementById("vLink1").setAttribute("href", HTMLvalidLinkStr);
-    document.getElementById("vLink2").setAttribute("href", CSSvalidLinkStr);
+  var button = document.getElementById("addButton");
+  button.onclick = handleButtonClick;
+
+  loadPlaylist();
+  addValidation();
+}
+
+function addValidation() {
+  var loc = window.location.href;
+  var HTMLvalidLinkStr = 'http://validator.w3.org/check?uri=' + loc;
+  var CSSvalidLinkStr = 'http://jigsaw.w3.org/css-validator/validator?uri=' +
+                        loc + '?profile=css3';
+  document.getElementById("vLink1").setAttribute("href", HTMLvalidLinkStr);
+  document.getElementById("vLink2").setAttribute("href", CSSvalidLinkStr);
+}
+
+function handleButtonClick(e) {
+  var textInput = document.getElementById("songTextInput");
+  var songName = textInput.value;
+
+  if (songName == "") {
+    alert("Please enter a song");
   }
-  window.onload = init;
+  else {
+    var li = document.createElement("li");
+    li.innerHTML = songName;
+    var ul = document.getElementById("playlist");
+    ul.appendChild(li);
+    save(songName);
+    textInput.value = "";
+  }
+}
